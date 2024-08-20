8cbbad05b7761b5eee4be78c82bf81282868c694
hparupudi <119982677+hparupudi@users.noreply.github.com>
2024-08-20 10:52:10 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
+import logging
 from flask import Flask, jsonify, request, session
+from datetime import timedelta
+from flask_cors import CORS, cross_origin
+from flask_session import Session
 import os
+
+
 from dotenv import load_dotenv
 from passlib.hash import pbkdf2_sha256
 import pymongo
@@ -16,19 +22,25 @@ from langchain.chains.combine_documents import create_stuff_documents_chain
 from langchain.chains.structured_output import create_structured_output_runnable
 from langchain_core.prompts import PromptTemplate, ChatPromptTemplate
 
-app = Flask(__name__)
-app.secret_key = os.getenv("SECRET_KEY", "default_secret_key")
-
 load_dotenv()
 
-client = pymongo.MongoClient(os.getenv("MONGO_URI", "mongodb://localhost:27017"))
+app = Flask(__name__)
+app.config["SECRET_KEY"] = os.getenv("SECRET_KEY")
+app.config["SESSION_TYPE"] = "filesystem"
+app.permanent_session_lifetime = timedelta(days=5)
+app.config.update(
+    SESSION_COOKIE_SAMESITE="None",
+    SESSION_COOKIE_SECURE=False  # Disable this for local development
+)
+
+Session(app)
+CORS(app, supports_credentials=True, resources={r"/*": {"origins": "http://localhost:3000/"}})
+
+client = pymongo.MongoClient(os.getenv("MONGO_URI"))
 db = client.login
 
 openai_api_key = os.getenv("OPENAI_API_KEY")
 pinecone_api_key = os.getenv("PINECONE_API_KEY")
-
-if not openai_api_key or not pinecone_api_key:
-    raise ValueError("Required API keys are missing")
 
 llm = OpenAI(model="gpt-3.5-turbo-instruct", temperature=0.9, max_tokens=3000, openai_api_key=openai_api_key)
 embeddings = OpenAIEmbeddings(model="text-embedding-3-large", openai_api_key=openai_api_key)
@@ -36,70 +48,52 @@ index_name = "recipes"
 docsearch = PineconeVectorStore.from_existing_index(index_name=index_name, embedding=embeddings)
 
 system_prompt = (
-    """Use the given context to generate custom recipes for the user. You should generate a unique recipe name, the ingredients needed to make
-    the recipe, the steps to prepare the recipe, and the recipe's nutritional information along with the serving size, nothing else. Use the exact headings 'Recipe Name:', 'Ingredients:', 'Steps:', and 'Nutrition Info:' before each respective part of the response. """
-    """Dataset: \n{context}\n"""
-)
+        """Use the given context to generate custom recipes for the user. You should generate a unique recipe name, the ingredients needed to make
+        the recipe, the steps to prepare the recipe, and the recipe's nutritional information along with the serving size, nothing else. Use the exact headings 'Recipe Name:', 'Ingredients:', 'Steps:', and 'Nutrition Info:' before each respective part of the response. 
+        Ensure that all the ingredients include quantities and units. All the ingredients must be appropriate.
+        Do not return any recipes that could be potentially dangerous or innapropriate.
+        Under "Nutrition Info", include the following: "Serving Size", "Total Calories", "Carbohydrates", "Protein", and "Fat. Only use a colon ":" and nothing else to show the values for each of the above.
+        They should be in the following format:
+        Serving Size: X people
+        Calories: X
+        Carbohydrates: Xg
+        Protein: Xg
+        Fats: Xg
+        You may only use hyphens "-" before each ingredient and you may not use hyphens "-" inside each ingredient."""
+        """Dataset: \n{context}\n"""
+    )
 
 prompt = ChatPromptTemplate.from_messages(
-    messages=[
-        ("system", system_prompt),
-        ("human", "{input}")
-    ],
-)
-
-@app.route("/api/recipes", methods=["GET", "POST"])
-def recipes():
-    if request.method == "POST":
-        user_prompt = request.get_json()
-        user_prompt = user_prompt.get("prompt")
-        
-        retriever = docsearch.as_retriever(search_time="similarity", search_kwargs={"k": 2})
-        qa_chain = create_stuff_documents_chain(llm=llm, prompt=prompt)
-        chain = create_retrieval_chain(retriever, qa_chain)
-
-        response = chain.invoke({"input": user_prompt})
-        
-        user_info = session.get('user', {})
-        email = user_info.get('email', 'Email not found')
-
-        curr_user = db.users.find_one({"email": email})
-
-        if curr_user and "recipe" in curr_user:
-            db.users.update_one(
-                {"email": email},
-                {"$push": {"recipe": response["answer"]}}
-            )
-        else:
-            db.users.update_one(
-                {"email": email},
-                {"$set": {"recipe": [response["answer"]]}}
-            )
-
-        return jsonify(response["answer"])
+        messages=[
+            ("system", system_prompt),
+            ("human", "{input}")
+        ],
+    )
 
 @app.route("/api/signup", methods=["POST"])
+@cross_origin(supports_credentials=True)
 def signup():
+    session.permanent = True
     user = {
         "name": request.form.get("name"),
         "email": request.form.get("email"),
-        "password": request.form.get("password")
+        "password": request.form.get("password"),
+        "recipes": [],
     }
 
     user["password"] = pbkdf2_sha256.hash(user["password"])
 
     if db.users.find_one({"email": user['email']}):
-        return jsonify({"error": "Account already exists"})
+         return jsonify({"error": "Account already exists"})
 
     db.users.insert_one(user)
     user_json = json.loads(json_util.dumps(user))
     del user['password']
-    session["logged_in"] = True
-    session["user"] = user_json
 
-    return jsonify(session), 200
+    return jsonify(session["logged_in"]), 200
 
 @app.route("/api/login", methods=["POST"])
+@cross_origin(supports_credentials=True)
 def login():
     user = db.users.find_one({
         'email': request.form.get('email')
@@ -110,10 +104,46 @@ def login():
         del user_json['password']
         session["logged_in"] = True
         session["user"] = user_json
-        
-        return jsonify(user_json), 200
+        session.modified = True
+
+        return jsonify(session["user"]), 200
 
     return jsonify({"error": "invalid login credentials"}), 401
 
+@app.route("/api/logout", methods=["GET"])
+@cross_origin(supports_credentials=True)
+def logout():
+    session.pop("user", None)
+    session["logged_in"] = False
+    return jsonify(session["logged_in"]), 200
+
+@app.route("/api/recipes", methods=["GET", "POST"])
+def recipes():
+    if request.method == "POST":
+        user_prompt = request.get_json()
+        user_prompt = user_prompt.get("prompt")
+            
+        retriever = docsearch.as_retriever(search_time="similarity", search_kwargs={"k": 2})
+        qa_chain = create_stuff_documents_chain(llm=llm, prompt=prompt)
+        chain = create_retrieval_chain(retriever, qa_chain)
+
+        response = chain.invoke({"input": user_prompt})
+        
+        user = session.get('user', {})
+        print(user)
+
+        if user:
+            email = user.get('email', 'email not found')
+            print(email)
+            curr_user = db.users.find_one({"email": email})
+
+            if curr_user and "recipes" in curr_user:
+                db.users.update_one(
+                {"email": email},
+                {"$push": {"recipes": response["answer"]}}
+                )
+
+        return jsonify(response["answer"])
+
 if __name__ == "__main__":
-    app.run(debug=True, port=8080)
+    app.run(host="0.0.0.0", debug=True, port=8080)
